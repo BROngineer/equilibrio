e1d2bd006e565229d7adbada56201356b5fedafd
Artem Bortnikov <brongineer747@gmail.com>
2024-10-27 22:52:46 +0200

    sample args
    Signed-off-by: Artem Bortnikov <brongineer747@gmail.com>

--- src/cmd/args.rs
+++ src/cmd/args.rs
@@ -1,14 +1,39 @@
-use clap::Parser;
+use std::net::SocketAddr;
+use clap::{Arg, Command};
 
-#[derive(Parser)]
-#[command(version = "1.0", author = "Artem Bortnikov <brongineer747@gmail.com>")]
-pub struct Args {
-    #[arg(short, long)]
-    pub address: String,
-    
-    #[arg(short, long)]
-    pub port: u16,
-    
-    #[arg(short, long)]
-    pub endpoint: String,
+pub struct Config {
+    pub bind_address: SocketAddr,
+    pub endpoint: SocketAddr,
+}
+
+pub fn parse() -> Config {
+    let matches = Command::new("equilibrio")
+        .version("0.1.0")
+        .arg(
+            Arg::new("bind-address")
+                .long("bind-address")
+                .required(false)
+                .default_value("127.0.0.1")
+        )
+        .arg(Arg::new("bind-port")
+            .long("bind-port")
+            .required(false)
+            .default_value("9080")
+        )
+        .arg(Arg::new("endpoint")
+            .short('e')
+            .long("endpoint")
+            .required(true)
+        ).get_matches();
+
+    let bind_addr = matches.get_one::<String>("bind-address").expect("Missing bind-address argument").to_string();
+    let bind_port = matches.get_one::<String>("bind-port").expect("Missing bind-port argument").to_string();
+    let endpoint_addr = matches.get_one::<String>("endpoint").expect("Missing endpoint argument").to_string();
+
+    let bind_address = format!("{}:{}", bind_addr, bind_port).parse().expect("Invalid bind address");
+    let endpoint = endpoint_addr.parse().expect("Invalid endpoint address");
+    Config {
+        bind_address,
+        endpoint,
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use std::net::SocketAddr;
-use clap::Parser;
 use tokio::io::copy;
 use tokio::net::{TcpListener, TcpStream};
 
-use equilibrio::cmd::args::Args;
+use equilibrio::cmd::args::parse;
 
-async fn forward(mut inbound: TcpStream, addr: SocketAddr) -> tokio::io::Result<()> {
-    let mut outbound = TcpStream::connect(addr).await?;
+async fn forward(inbound: TcpStream, addr: SocketAddr) -> tokio::io::Result<()> {
+    let outbound = TcpStream::connect(addr).await?;
 
-    let (mut ri, mut wi) = inbound.split();
-    let (mut ro, mut wo) = outbound.split();
+    let (mut ri, mut wi) = inbound.into_split();
+    let (mut ro, mut wo) = outbound.into_split();
 
     let client_to_server = copy(&mut ri, &mut wo);
     let server_to_client = copy(&mut ro, &mut wi);
@@ -21,18 +20,13 @@ async fn forward(mut inbound: TcpStream, addr: SocketAddr) -> tokio::io::Result<
 
 #[tokio::main]
 async fn main() -> tokio::io::Result<()> {
-    let args = Args::parse();
-    
-    let bind_addr: SocketAddr = format!("{}:{}", args.address, args.port)
-        .parse().expect("Invalid socket address");
-    let proxy_addr: SocketAddr = args.endpoint.parse().expect("Invalid socket address");
+    let args = parse();
 
-
-    let listener = TcpListener::bind(bind_addr).await?;
+    let listener = TcpListener::bind(args.bind_address).await?;
     loop {
         let (inbound, _) = listener.accept().await?;
         tokio::spawn(async move {
-            if let Err(e) = forward(inbound, proxy_addr).await {
+            if let Err(e) = forward(inbound, args.endpoint).await {
                 eprintln!("Error handling connection: {}", e);
             }
         });
